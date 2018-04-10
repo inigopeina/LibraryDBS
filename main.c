9c8d1bb4964fb192a4fe104782f80357c42d5d72
Iñigo <inigopeina@opendeusto.es>
2018-04-10 08:44:53 +0200

    eliminado metodo borrar libro, para usuario

--- main.c
+++ main.c
@@ -20,7 +20,6 @@ int menuUsuario(void)
 		
 		printf("1. Lista de libros disponibles para alquilar\n");
 		printf("2. Alquilar libro\n");
-		printf("3. Eliminar libro\n");
 		printf("4. EXIT\n");
 		printf("\n");
 		
@@ -151,10 +150,7 @@ int main(int argc, char **argv, char **vectorUsuarios)
 							alquilarLibro();
 							break;
 
-						case 3:
-
-							eliminarLibro();
-							break;
+					
 							
 
 						default:
